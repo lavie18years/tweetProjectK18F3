976060a19fa5c0b68cad14dc512afbd5428535ba
nguynphmnaman@gmail.com <nguynphmnaman@gmail.com>
2023-11-01 18:52:51 +0700

    bat dau buoi 30

--- src/middlewares/error.middlewares.ts
+++ src/middlewares/error.middlewares.ts
@@ -6,14 +6,16 @@ import { ErrorWithStatus } from '~/models/Error'
 export const defaultErrorHandle = (err: any, req: Request, res: Response, next: NextFunction) => {
   // đây là nơi mà tất cả những lỗi trên toàn hệ thống sẽ dồn về
   if (err instanceof ErrorWithStatus) {
-    res.status(err.status).json(omit(err, ['status']))
+    return res.status(err.status).json(omit(err, ['status']))
   }
   // nếu mà lỗi xuống đc đây
   Object.getOwnPropertyNames(err).forEach((key) => {
+    // for in ko duyệt đc enumerable là false
     Object.defineProperty(err, key, { enumerable: true })
   })
   res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
     message: err.message,
     errorInfor: omit(err, ['stack'])
+    // ko cho quăng stack vì nó chứa nhiều thông tin nguy hiểm
   })
 }
